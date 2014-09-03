31abacdf23efba19308e94b4156f5af33e478569
vacant697 <vacant697@gmail.com>
2014-09-03 20:28:51 +0800

    new file:   public/favicon.ico 	new file:   public/images/logo.png 	new file:   public/stylesheets/base.css 	new file:   public/stylesheets/common.css 	new file:   public/stylesheets/font.css 	new file:   public/stylesheets/layout.css 	deleted:    public/stylesheets/style.css 	modified:   routes/index.js 	modified:   views/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '空灵的前端日记' });
 });
 
 module.exports = router;
